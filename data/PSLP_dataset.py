0e30d5e3f377fe191948c640a889f5f85a8eafdd
mcw92 <marie.weiel@kit.edu>
2023-02-02 14:52:18 +0100

    implemented PSLP class from notebook snippets

--- data/PSLP_dataset.py
+++ data/PSLP_dataset.py
@@ -1,27 +1,42 @@
-import pandas as pd
-import numpy as np
-import holidays
-import plotly.express as px
-pd.set_option('display.max_rows', 100)
-
-def PSLPDataset():
+class PSLPDataset:
     """
     Class for fetching and preprocessing ENTSO-E load and generation data.
     """
-    def __init__(start_date,
-                 end_date,
-                 api_key, # web token for RESTful API
-                 country_code = "10Y1001A1001A83F", # Germany
-                 time_zone = "Europe/Berlin", # time zone for Germany
+    def __init__(
+        self,
+        start_date, 
+        end_date,                          # overall end date
+        api_key, # web token for RESTful API
+        country_code = "10Y1001A1001A83F", # country code (default: Germany)
+        time_zone = "Europe/Berlin" # time zone for Germany
                 ):
         """
+        Initialize PSLP dataset.
+        Params
+        ------
+        start_date : str
+                     overall start date as "YYYYMMDD"
+        end_date : str
+                   overall end date as "YYYYMMDD"
+        api_key : str
+                  web token for RESTfulAPI access to ENTSO-E transparency platform
+        country_code : str
+                       country code
+        time zone : str
+                    time zone
         """
         self.start_date = start_date
         self.end_date = end_date
         self.country = country_code
         self.time_zone = time_zone
         self.api_key = api_key
-        
+        self.df = None
+        self.original_headers = None
+        self.errors = None
+        self.fetch_data()
+        self.calculate_pslps()
+        self.calculate_residuals()
+        self.calculate_errors()
     
     def _get_load_intervals(self):
         """
@@ -63,8 +78,6 @@ def PSLPDataset():
     def _load_data(self, start_date, end_date):
         """
         Load actual load and actual aggregated generation per production type for requested time interval.
-        
-        
         Params
         ------
         start_date : str
@@ -95,17 +108,13 @@ def PSLPDataset():
     
     def fetch_data(self, drop_consumption=True):
         """
-        Fetch data from ENTSO-E transparency platform as requested.
+        Fetch actual load and generation per type from ENTSO-E transparency platform 
+        for requested time interval. Set resulting dataframe as attribute.
         
         Parameters
         ----------
         drop_consumption : Bool
                            Drop columns containing actual consumption.
-        create_pslp_columns : Bool
-                              Create columns for subsequent PSLP and residuals calculation.    
-        Returns
-        -------
-        pd.DataFrame with actual load and generation per type for requested time interval
         """
         # Determine sequence of dates to consider when loading data.
         dates = self._get_load_intervals()
@@ -115,7 +124,6 @@ def PSLPDataset():
         for i, _ in enumerate(dates):
     
             if i == dates.shape[0] - 1:
-                print("Returning final data frame...")
                 df_final = pd.concat(df_list, axis=0) # Concatenate dataframes along time axis (index).
                 df_final.index = pd.to_datetime(df_final.index, utc=True).tz_convert(tz="UTC+01:00")
     
@@ -129,8 +137,10 @@ def PSLPDataset():
                 for header in original_headers:
                     df_final[str(header) + " PSLP"] = pd.Series(dtype='float')
                 
+                print("Returning final data frame...")
                 self.df = df_final
                 self.original_headers = original_headers
+                return
                 
             try:
                 print(f"Trying to load data chunk for time interval [{dates[i]}, {dates[i+1]}]...")
@@ -143,7 +153,7 @@ def PSLPDataset():
                 print(f"Loading failed!", e)
                 continue
 
-                
+    @staticmethod            
     def get_pslp_category(date, weekday=None, holiday=None, country_code='DE'):
         """
         Get PSLP category from date, weekday information, and holiday information.
@@ -193,7 +203,7 @@ def PSLPDataset():
         return pslp_category
     
     
-    def assign_pslp_categories(df, country_code='DE'):
+    def _assign_pslp_categories(self, country_code="DE"):
         """
         Assign PSLP categories to dates in dataframe's datetime index.
     
@@ -216,21 +226,20 @@ def PSLPDataset():
         # Get holidays in specified country.
         country_holidays = holidays.country_holidays(country_code) # Passing a state is also possible!
     
-        s = df.index.to_series()                                # Convert datetime index to series.
+        s = self.df.index.to_series()                                # Convert datetime index to series.
         dates = s.dt.date                                       # Get plain dates from datetime objects.
         weekdays = s.dt.weekday                                 # Get weekdays from datetime objects.
         holidays = [date in country_holidays for date in dates] # Determine holidays.
         pslp_category = []
         
         for d, wd, hd in zip(dates, weekdays, holidays):
-            pslp_category.append(get_pslp_category(d, wd, hd))
+            pslp_category.append(self.get_pslp_category(d, wd, hd))
             
-        df["PSLP Category"] = pslp_category
-        df["Holiday"] = holidays
-        df["Weekday"] = weekdays
-        return df
+        self.df["PSLP Category"] = pslp_category
+        self.df["Holiday"] = holidays
+        self.df["Weekday"] = weekdays
     
-    
+    @staticmethod
     def _get_nearest_future_pslp_date(date_str, pslp_category=None):
         """
         For a given date, get nearest days in future for each PSLP category.
@@ -241,18 +250,18 @@ def PSLPDataset():
                    considered date
         """
         if pslp_category is None:
-            pslp_category = get_pslp_category(date_str)
+            pslp_category = PSLPDataset.get_pslp_category(date_str)
         start = pd.to_datetime(date_str).date() + pd.Timedelta(days=1)
         end = pd.to_datetime(date_str).date() + pd.Timedelta(weeks=1)
         future_dates = pd.date_range(start=start.strftime('%Y%m%d'), end=end.strftime('%Y%m%d'))
-        pslp_categories = np.array([get_pslp_category(d) for d in future_dates])
+        pslp_categories = np.array([PSLPDataset.get_pslp_category(d) for d in future_dates])
         idx = np.where(pslp_categories == pslp_category)
         return future_dates[idx][0].date()
     
 
-    def _calculate_pslp(df, original_headers, date_str, lookback=3, country_code='DE', DEBUG=False):
+    def _calculate_pslp(self, date_str, lookback=3, country_code='DE', DEBUG=False):
         """
-        Calculate PSLPs for all dates in dataframe or for given date from given data.
+        Calculate PSLP for given date from given data.
         
         The data is categorized into weekdays, Saturdays, and Sundays/holidays.
         The `lookback` most recent days from the specified date's category are used to
@@ -260,10 +269,6 @@ def PSLPDataset():
         
         Params
         ------
-        df : pandas.Dataframe
-             data to calculate PSLP for, must have datetime index
-        original_headers : list of str
-                           categories to calculate PSLP for
         date_str : str
                    date 'YYYYMMDD' to calculate PSLP for; if None, calculate PSLP for all dates
         lookback : int
@@ -272,14 +277,16 @@ def PSLPDataset():
                        considered country (for holidays)
         
         """
-        unique_dates = df.index.to_series().dt.date.drop_duplicates().tolist() # Get unique dates in data index.
-        df = assign_pslp_categories(df, country_code)
+        unique_dates = self.df.index.to_series().dt.date.drop_duplicates().tolist() # Get unique dates in data index.
+        self._assign_pslp_categories(country_code)
     
-        print(f"Calculating PSLP for date {date_str}...")
+        if DEBUG:
+            print(f"Calculating PSLPs for date {date_str}...")
         date = pd.to_datetime(date_str)
         
-        pslp_category = get_pslp_category(date_str)
-        print(f"PSLP category of {date.date()} is {pslp_category}.")
+        pslp_category = self.get_pslp_category(date_str)
+        if DEBUG:
+            print(f"PSLP category of {date.date()} is {pslp_category}.")
     
         # Check whether date is in range of given dataframe.
         if date.date() < unique_dates[0]:
@@ -288,20 +295,22 @@ def PSLPDataset():
             raise IndexError(f"PSLP cannot be calculated. Date {date_str} is too far in the future.")
         assert date.date() in unique_dates
         
-        unique_dates_pslp = df[df['PSLP Category'] == pslp_category].index.to_series().dt.date.drop_duplicates().tolist()
+        unique_dates_pslp = self.df[self.df['PSLP Category']==pslp_category].index.to_series().dt.date.drop_duplicates().tolist()
         idx_pslp = unique_dates_pslp.index(date.date())
-        print(f"Index in unique days of PSLP category is {idx_pslp}.")
+        if DEBUG:
+            print(f"Index in unique days of PSLP category is {idx_pslp}.")
         if idx_pslp - lookback < 0:
             raise IndexError(f"PSLP cannot be calculated. Less than {lookback} samples in PSLP category for date {date_str}.")
         lookback_dates = [pd.to_datetime(d).strftime('%Y-%m-%d') for d in unique_dates_pslp[idx_pslp-lookback:idx_pslp]]
-        print(f"Dates to consider for calculating PSLP: {lookback_dates}")
-        for header in original_headers:
-            print(f"{header}...")
-            df[header+" PSLP"].at[date_str] = pd.concat([df[header].at[d].reset_index(drop=True) for d in lookback_dates], axis=1).mean(axis=1)
-        return df
+        if DEBUG:
+            print(f"Dates to consider for calculating PSLP: {lookback_dates}")
+        for header in self.original_headers:
+            if DEBUG:
+                print(f"{header}...")
+            self.df[header+" PSLP"].at[date_str] = pd.concat([self.df[header].at[d].reset_index(drop=True) for d in lookback_dates], axis=1).mean(axis=1)
     
     
-    def calculate_pslps(df, original_headers, date_str=None, lookback=3, country_code='DE', DEBUG=False):
+    def calculate_pslps(self, date_str=None, lookback=3, country_code='DE', DEBUG=False):
         """
         Calculate PSLPs for all dates in dataframe or for given date from given data.
         
@@ -325,79 +334,64 @@ def PSLPDataset():
         """
         if date_str is not None:
             print(f"Calculating PSLP for date {date_str} only...")
-            df = _calculate_pslp(df, 
-                                original_headers, 
-                                date_str, 
-                                lookback=lookback, 
-                                country_code=country_code, 
-                                DEBUG=DEBUG)
+            self._calculate_pslp(date_str, 
+                                 lookback=lookback, 
+                                 country_code=country_code, 
+                                 DEBUG=DEBUG)
         
         else:
             print("Calculating PSLPs for all dates in dataframe...")
-            unique_dates = df.index.to_series().dt.date.drop_duplicates().tolist() # Get unique dates in data index.
+            unique_dates = self.df.index.to_series().dt.date.drop_duplicates().tolist() # Get unique dates in data index.
             unique_dates = [pd.to_datetime(d).strftime('%Y-%m-%d') for d in unique_dates]
             
-            for date in unique_dates:
+            for date in tqdm(unique_dates):
                 try:
-                    df = _calculate_pslp(df, 
-                                        original_headers, 
-                                        date_str=date, 
-                                        lookback=lookback, 
-                                        country_code=country_code, 
-                                        DEBUG=DEBUG)
+                    self._calculate_pslp(date_str=date, 
+                                         lookback=lookback, 
+                                         country_code=country_code, 
+                                         DEBUG=DEBUG)
                 except IndexError as e:
                     print(e)
-        return df
     
     
-    def calculate_residuals(df, original_headers):
+    def calculate_residuals(self):
         """
         Calculate residuals of actual data w.r.t PSLPs.
         """
-        for header in original_headers:
-            df[header+" Residuals"] = df[header] - df[header+" PSLP"]
-        return df
+        for header in self.original_headers:
+            self.df[header+" Residuals"] = self.df[header] - self.df[header+" PSLP"]
     
     
-    def plot_data(df, original_headers):
+    def plot_data(self):
         """
         Plot preprocessed load and generation data.
-        
-        Params
-        ------
-        df : pandas.DataFrame
-             pre-processed data with PSLPs and residuals
-        original_headers : list of str
-                           original headers in ENTSO-E dataframe
         """
         from plotly.subplots import make_subplots
         import plotly.graph_objects as go
     
-        num_rows = len(original_headers)
+        num_rows = len(self.original_headers)
         
-        fig = make_subplots(rows=num_rows, cols=1, subplot_titles=(original_headers))
+        fig = make_subplots(rows=num_rows, cols=1, subplot_titles=(self.original_headers))
     
-        for i, header in enumerate(original_headers):
-            fig.add_trace(go.Scatter(x = df.index, y = df[header], name=header), row=i+1, col=1)
-            fig.add_trace(go.Scatter(x = df.index, y = df[header+" PSLP"], name=header+" PSLP"), row=i+1, col=1)
-            fig.add_trace(go.Scatter(x = df.index, y = df[header+" Residuals"], name=header+" Residuals"), row=i+1, col=1)
+        for i, header in enumerate(self.original_headers):
+            fig.add_trace(go.Scatter(x = self.df.index, y = self.df[header], name=header), row=i+1, col=1)
+            fig.add_trace(go.Scatter(x = self.df.index, y = self.df[header+" PSLP"], name=header+" PSLP"), row=i+1, col=1)
+            fig.add_trace(go.Scatter(x = self.df.index, y = self.df[header+" Residuals"], name=header+" Residuals"), row=i+1, col=1)
     
         fig.update_layout(height=10000, width=1200)
         fig.show()
         
        
-    def calculate_errors(df, original_headers):
+    def calculate_errors(self):
         """
         Calculate forecasting errors for preprocessed ENTSO-E load and generation data.  
         """
-        import sklearn
-        for header in original_headers:
-            temp = pd.concat([df[header], df[header+" PSLP"]], axis=1).dropna()
-            #print(temp)
-            mae = sklearn.metrics.mean_absolute_error(temp[header], temp[header+" PSLP"])
-            mape = sklearn.metrics.mean_absolute_percentage_error(temp[header], temp[header+" PSLP"])
-            mse = sklearn.metrics.mean_squared_error(temp[header], temp[header+" PSLP"])
-            print_str = f"{header}:\n"
-            print_str += f"MAE = {mae}\nMSE = {mse}\nMAPE = {mape}\n"
-            print(print_str)
-            #print(df.shape, temp.shape)
+        from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
+        errors = {}
+        for header in self.original_headers:
+            temp = pd.concat([self.df[header], self.df[header+" PSLP"]], axis=1).dropna()
+            mae = mean_absolute_error(temp[header], temp[header+" PSLP"])
+            mape = mean_absolute_percentage_error(temp[header], temp[header+" PSLP"])
+            mse = mean_squared_error(temp[header], temp[header+" PSLP"])
+            errors[header] = {"MAE": mae, "MAPE": mape, "MSE": mse}
+        self.errors = errors
